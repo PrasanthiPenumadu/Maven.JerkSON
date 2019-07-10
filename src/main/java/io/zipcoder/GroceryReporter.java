b8f91ba99d9797353892b37d8d8e42590cb8a612
prasanthipenmadu <prasanthipl@gmail.com>
2019-07-10 08:10:48 -0400

    Still working on toString

--- src/main/java/io/zipcoder/GroceryReporter.java
+++ src/main/java/io/zipcoder/GroceryReporter.java
@@ -1,6 +1,12 @@
 package io.zipcoder;
 
 import io.zipcoder.utils.FileReader;
+import io.zipcoder.utils.Item;
+import io.zipcoder.utils.ItemParseException;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.regex.Pattern;
 
 public class GroceryReporter {
     private final String originalFileText;
@@ -8,9 +14,72 @@ public class GroceryReporter {
     public GroceryReporter(String jerksonFileName) {
         this.originalFileText = FileReader.readFile(jerksonFileName);
     }
+public List<Integer> numOfOcc() {
+        Integer milk=0;
+        Integer p1=0;
+        Integer p2=0;
+        Integer error=0;
+        ItemParser ip=new ItemParser();
+    List<Item> list=new ArrayList<>();
+    String delims="##";
+    Pattern pattern=Pattern.compile(delims,Pattern.CASE_INSENSITIVE);
+    String[] al=pattern.split(originalFileText);
+    try {
+        for(int i=0;i<al.length;i++){
+            if(al[i].length()>51)
+                list.add(ip.parseSingleItem(al[i]));}
+    } catch (ItemParseException e) {
+        e.printStackTrace();
+    }
+    System.out.println(list.size());
 
+
+    for(Item i:list) {
+           if (i.getName().equals("milk"))
+               milk++;
+           { if (i.getPrice() == 3.23)
+                   p1++;
+               else if (i.getPrice() == 1.23)
+                   p2++;
+               else
+                   error++; }}
+       List<Integer> alist=new ArrayList<>();
+       alist.add(milk);
+       alist.add(p1);
+       alist.add(p2);
+       alist.add(error);
+       return alist;
+}
+    public List<Integer> numOfOccBread(){
+        Integer bread=0;
+        Integer p1=0;
+        Integer error=0;
+        ItemParser ip=new ItemParser();
+        List<Item> list=ip.parseItemList(originalFileText);
+        for(Item i:list) {
+            if (i.getName().equals("bread"))
+                bread++;
+            { if (i.getPrice() == 1.23)
+                p1++;
+            else
+                error++; }}
+        List<Integer> alist=new ArrayList<>();
+        alist.add(bread);
+        alist.add(p1);
+        alist.add(error);
+        return alist;
+    }
     @Override
     public String toString() {
-        return null;
+        System.out.println(numOfOcc());
+        System.out.println(numOfOccBread());
+        return new StringBuilder()
+
+                .append("\nname:    Milk        seen: " + numOfOcc().get(0)+" times")
+                .append("\n=============        =============")
+                .append("\nPrice:   3.23        seen: " + numOfOcc().get(1)+" times")
+                .append("\n-------------        -------------")
+                .append("\nPrice:   1.23        seen: " + numOfOcc().get(2)+" times")
+                .toString();
     }
 }
--- src/main/java/io/zipcoder/ItemParser.java
+++ src/main/java/io/zipcoder/ItemParser.java
@@ -11,54 +11,51 @@ import java.util.regex.Pattern;
 
 public class ItemParser {
     public List<Item> parseItemList(String valueToParse) {
-       String delims="[##]";
-        String[] al=valueToParse.split(delims);
+       String delims="##";
+        Pattern pattern=Pattern.compile(delims,Pattern.CASE_INSENSITIVE);
+        String[] al=pattern.split(valueToParse);
+        List<Item> itemList=new ArrayList<>();
+        try {
+            for(int i=0;i<al.length;i++){
+                if(al[i].length()>50)
+            itemList.add(parseSingleItem(al[i]));}
+        } catch (ItemParseException e) {
+            e.printStackTrace();
+        }
 
-        String pS="nAme:";
-       Integer j=0;
-        //String x=valueToParse.replace("##", ",");
-        Pattern pattern=Pattern.compile(pS);
-        Matcher matcher=pattern.matcher(valueToParse);
-        for(int i=0;matcher.find();i++)
-            j=matcher.end();
-            System.out.println(matcher.end());
-            List<String> list=Arrays.asList(al);
-           // List<Item> alist= new ArrayList<Item>(list) ;
 
-        return null;
+        return itemList;
     }
 
     public Item parseSingleItem(String singleItem) throws ItemParseException {
         String delims="[;]";
-       // String[] al=singleItem.split(delims);
         Pattern pattern=Pattern.compile(delims,Pattern.CASE_INSENSITIVE);
         String singleItem1=singleItem.toLowerCase();
         String[] al=pattern.split(singleItem1);
-//        for(String x:al){
-//           // if(x.())
-//            String pS="nAme:";
-//            Integer j=0;
-//            Pattern pattern1=Pattern.compile(pS);
-//            Matcher matcher=pattern1.matcher(x);
-//            for(int i=0;matcher.find();i++)
-//                j=matcher.end();
-//            System.out.println(matcher.end());
-//        }
-        Pattern p=Pattern.compile("\\d+");
+        String nameItem="";
+        String pri="";
+        String type="";
+        String date="";
+        Pattern pname=Pattern.compile("^[@:\\^*%]");
+        for(int i=0;i<al.length;i++){
+            String all[]=al[i].split("[@:\\^*%]");
+            if(all.length!=2)
+                throw new io.zipcoder.utils.ItemParseException();
+             if(all[0].equalsIgnoreCase("name")){
+                nameItem=all[1];}
+            else if(all[0].equalsIgnoreCase("price"))
+               pri =all[1];
+            else if(all[0].equalsIgnoreCase("type"))
+               type =all[1];
+            else if(all[0].equalsIgnoreCase("expiration"))
+                date =all[1];
+        }
 
-//        Pattern pname=Pattern.compile("\\naMe:+");
-//        String name=pname.matcher(al[0]).group(1);
-//        System.out.println(name);
-String x=al[0].replace("name:", "");
-        Double d= Double.valueOf((al[1]).replaceAll("\\D+",""));
-//Double d= Double.valueOf(p.matcher(al[1]).group());
-        System.out.println(d/100);
-String y=al[2].replace("type:", "");
-String z=al[3].replace("##", "");
-String z1=z.replace("expiration:", "");
-//        System.out.println(x+" "+al[1]+" "+y+" "+z);
-//        String res=(x+" "+ p.matcher(al[1]).group()+" "+y+" "+ p.matcher(al[3]).group());
-        Item i=new Item(x,d/100,y,z1);
+        Double d= Double.valueOf(pri);
+        Pattern pattern1 = Pattern.compile("[##]");
+        Matcher matcher = pattern1.matcher(date);
+        String number = matcher.replaceAll("");
+        Item i=new Item(nameItem,d,type,number);
 
         return i;
     }
